16abbcfac7a447a22fd730624585710ef017c2f0
Bilal Tufail Khan <66962629+bilaltufailkhan@users.noreply.github.com>
2022-04-26 13:08:35 +0500

    First Commit

---
+++ src/views/MainView.js
@@ -0,0 +1,17 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const AdminView = (props) => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Yield Box</h1>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default AdminView;
