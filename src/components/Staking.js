4f90d447ae8078db44cce617bbebd0beedcbe249
Bilal Tufail Khan <66962629+bilaltufailkhan@users.noreply.github.com>
2022-04-28 04:10:27 +0500

    Staking Component

--- src/components/Staking.js
+++ src/components/Staking.js
@@ -1,13 +1,59 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import apy from "../assets/img/apy.svg";
+import autostaking from "../assets/img/autostaking.svg";
+import reflection from "../assets/img/reflection.svg";
 
 const Staking = () => {
   return (
     <div className="staking__section">
-      <Container>
-        <Row className="d-flex justify-content-center align-items-center">
-          <Col xs="12" className="vh-100 text-center">
-            <h1>Staking</h1>
+      <Container className="min-vh-100 d-flex">
+        <Row className="align-items-center vw-100">
+          <Col xs="12" className="text-center staking__content px-5">
+            <Container className="px-5">
+              <p>FIRST REBASE AUTO-STAKING WITH BUSD REFLECTION</p>
+              <h2>+385,945.80%</h2>
+              <p>
+                Paid every 15 minutes, and a simple buy-hold-earn system that
+                grows your $MINT portfolio in your wallet at a lightning fast
+                pace.
+              </p>
+              <Row className="justify-content-center">
+                <Col md="3" sm="12" className="my-3">
+                  <Card
+                    className="text-center"
+                    style={{ background: "transparent" }}
+                  >
+                    <Card.Body>
+                      <img src={apy} width="auto" height="50px" />
+                      <Card.Text>HIGHEST APY PROTOCOL</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col md="3" sm="12" className="my-3">
+                  <Card
+                    className="text-center"
+                    style={{ background: "transparent" }}
+                  >
+                    <Card.Body>
+                      <img src={autostaking} width="auto" height="50px" />
+                      <Card.Text>Auto-Stake Auto-Compound</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col md="3" sm="12" className="my-3">
+                  <Card
+                    className="text-center"
+                    style={{ background: "transparent" }}
+                  >
+                    <Card.Body>
+                      <img src={reflection} width="auto" height="50px" />
+                      <Card.Text>Hourly BUSD Reflection</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
           </Col>
         </Row>
       </Container>
