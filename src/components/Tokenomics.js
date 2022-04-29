e0d39ad8659e5d6350e4c76be6ddc32ea6f41c66
Bilal Tufail Khan <66962629+bilaltufailkhan@users.noreply.github.com>
2022-04-29 17:17:52 +0500

    Tokenomics + Earning

--- src/components/Tokenomics.js
+++ src/components/Tokenomics.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Table } from "react-bootstrap";
 
 const Tokenomics = () => {
   return (
     <div className="tokenomics__section">
-      <Container>
-        <Row className="d-flex justify-content-center align-items-center">
-          <Col xs="12" className="vh-100 text-center">
-            <h1>Tokenomics</h1>
+      <Container className="min-vh-100 d-flex">
+        <Row className="align-items-center vw-100">
+          <Col xs="12" className="text-center tokenomics__content">
+            <h2>Tokenomics</h2>
           </Col>
+        </Row>
+        <Row className="align-items-center vw-100">
+          <Col md="6">
+            <Table responsive="sm">
+              <thead></thead>
+            </Table>
+          </Col>
+          <Col md="6"></Col>
         </Row>
       </Container>
     </div>
