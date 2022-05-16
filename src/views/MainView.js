1742b4e18d4f8e2da3b6f8a29707f3a98195b181
bilaltechnoli <bilal.technoli@gmail.com>
2022-05-16 11:17:26 +0500

    Another push

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Col, Container, Row } from "react-bootstrap";
 import { FullPage, Slide } from "react-full-page/lib";
 import About from "../components/About";
@@ -18,60 +19,102 @@ import Nft from "../components/Nft";
 import SideNavigation from "../components/global/SideNavigation";
 
 const AdminView = (props) => {
-  return (
-    <div className="main__view">
-      <Navigation />
-      <SideNavigation />
-      <Container fluid>
-        <Row>
-          <Col>
-            <FullPage duration={100}>
-              <Slide>
-                <Slider />
-              </Slide>
-              <Slide>
-                <About />
-              </Slide>
-              <Slide>
-                <Staking />
-              </Slide>
-              <Slide>
-                <Earning />
-              </Slide>
-              <Slide>
-                <Tokenomics />
-              </Slide>
-              <Slide>
-                <Advantage />
-              </Slide>
-              <Slide>
-                <Rewards />
-              </Slide>
-              <Slide>
-                <Todo />
-              </Slide>
-              <Slide>
-                <Documents />
-              </Slide>
-              <Slide>
-                <Buy />
-              </Slide>
-              <Slide>
-                <MobileApp />
-              </Slide>
-              <Slide>
-                <Nft />
-              </Slide>
-              <Slide>
-                <Faqs />
-              </Slide>
-            </FullPage>
-          </Col>
-        </Row>
-      </Container>
-      <Footer />
-    </div>
-  );
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  const handleResize = (e) => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  });
+
+  React.useEffect(() => {
+    return () => {
+      window.addEventListener("resize", handleResize);
+    };
+  });
+  if (windowWidth > 540) {
+    return (
+      <div className="main__view">
+        <Navigation />
+        <SideNavigation />
+        <Container fluid>
+          <Row>
+            <Col>
+              <FullPage duration={100}>
+                <Slide>
+                  <Slider />
+                </Slide>
+                <Slide>
+                  <About />
+                </Slide>
+                <Slide>
+                  <Staking />
+                </Slide>
+                <Slide>
+                  <Earning />
+                </Slide>
+                <Slide>
+                  <Tokenomics />
+                </Slide>
+                <Slide>
+                  <Advantage />
+                </Slide>
+                <Slide>
+                  <Rewards />
+                </Slide>
+                <Slide>
+                  <Todo />
+                </Slide>
+                <Slide>
+                  <Documents />
+                </Slide>
+                <Slide>
+                  <Buy />
+                </Slide>
+                <Slide>
+                  <MobileApp />
+                </Slide>
+                <Slide>
+                  <Nft />
+                </Slide>
+                <Slide>
+                  <Faqs />
+                </Slide>
+              </FullPage>
+            </Col>
+          </Row>
+        </Container>
+        <Footer />
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className="main__view">
+        <Navigation />
+        <SideNavigation />
+        <Container fluid>
+          <Slider />
+          <About />
+          <Staking />
+          <Earning />
+          <Tokenomics />
+          <Advantage />
+          <Rewards />
+          <Todo />
+          <Documents />
+          <Buy />
+          <MobileApp />
+          <Nft />
+          <Faqs />
+        </Container>
+        <Footer />
+      </div>
+    )
+  }
+
 };
 
 export default AdminView;
